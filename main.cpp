812b9217234852e450ae6bf116ea4f5e991ac967
jarturoa <32652235+jarturoa@users.noreply.github.com>
2017-10-11 13:56:20 -0500

    main opciones 1, 2, 3.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include<fstream>
 #include <string>
-#include"Hora.h"
-#include"Pasajero.h"
-#include"Aeropuerto.h"
+#include "Hora.h"
+#include "Aeropuerto.h"
+#include "Vuelo.h"
+#include "Pasajero.h"
+
 using namespace std;
 const int MAXa = 5;
 const int MAXp = 15;
@@ -15,54 +17,88 @@ int main()
     string ArrPasajeros [MAXp];
     string ArrVuelos [MAXv];
     
-    string Linea1;
+    string Linea1, Linea2, Final1, Final2; //string linea guarda lo que se recibe del archivo, final guarda lo que se imprime en la pantalla final
+    int C1 = 0, C2=0, Length1 = 0, Length2 = 0, cs = 0;
+    
     
     //abrir archivos
     // abrir arhivo leer info txt uno
     ifstream archEnt1;
     archEnt1.open("Aeropuertos.txt");
+    
+    //abrir arhivo leer info txt dos
+    ifstream archEnt2;
+    archEnt2.open("Aeropuertos.txt");
+    
     //obtener lineas de primer archivo
     do{
         
         getline(archEnt1 , Linea1); //save the number in the corresponding array
         
-        iArrValues1 [C1] = Linea1;
+        ArrAeropuerto [C1] = Linea1;
         C1++;
-        Length1++; //know length of array
         
     } while (!archEnt1.eof());
     
+    //obtener lineas de segudo archivo
     
     
+    do{
+        getline(archEnt2 , Linea2); //save the number in the corresponding array
+        
+        ArrPasajeros [C2] = Linea2;
+        C2++;
+        
+    }while (!archEnt2.eof());
+    
+    do{
+        
+        cout << "Teclee los datos del un vuelo y precione enter" << endl;
+        
+        cin >> ArrVuelos [cs];
+        cs++;
+    }while (cs <= MAXv);
+    
+    cs=0;
     
     
+    int opcion;
     
-    
-int opcion;
-
- do {
-  cout << endl
-<< " 1 - Consulta de todos los vuelos.\n"
-<< " 2 - Dar de alta un pasajero en un vuelo.\n"
-<< " 3 - Consulta de vuelo por hora de salida\n"
-<< " 4 - Consulta por clave de vuelo\n"
-<< " 5 - Consulta de vuelos en un aeropuerto\n"
-<< " 6 - Consulta la lista de vuelos en los que viaja un pasajero\n"
-<< " 7 - Salir \n"
-<< " Ingrese su opcion : ";
-cin >> opcion;
-     
-switch (opcion)
-{
-case 1:
-
-break;
-case 2:
-
-break;
-case 3:
-
-break;
+    do {
+        cout << endl
+        << " 1 - Consulta de todos los vuelos.\n"
+        << " 2 - Dar de alta un pasajero en un vuelo.\n"
+        << " 3 - Consulta de vuelo por hora de salida\n"
+        << " 4 - Consulta por clave de vuelo\n"
+        << " 5 - Consulta de vuelos en un aeropuerto\n"
+        << " 6 - Consulta la lista de vuelos en los que viaja un pasajero\n"
+        << " 7 - Salir \n"
+        << " Ingrese su opcion : ";
+        cin >> opcion;
+        
+        switch (opcion)
+        {
+            case 1:
+                
+                do{
+                    
+                    cout << ArrVuelos [cs] << endl;
+                    cs++;
+                }while (cs>=MAXv);
+                
+                
+                break;
+            case 2:
+                cout << "Dame el numero de confirmacion" << endl;
+                cout << "Dame la clave del vuelo" << endl;
+                
+                
+                break;
+            case 3:
+                
+                cout << "Dame la hora en la que sale el vuelo" << endl;
+                cout << "Dame los minutos en los que sale el vuelo" << endl;
+                
 case 4:
 
 break;
